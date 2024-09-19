9e1a1be150b1e7fd61e82cfe2621d2e63df58ec7
jackobhat <jackobhat911@gmail.com>
2024-09-19 22:44:52 +0300

    updating the ui

--- components/mainLayout/header.tsx
+++ components/mainLayout/header.tsx
@@ -11,19 +11,23 @@ export default function Header() {
     <header className="flex items-center  flex-col ">
       <div className="pt-20 flex items-center justify-center flex-col">
         <div className="flex flex-col items-center justify-center">
-        <h2 className="text-white text-6xl text-center">Start selling travel today <p>on your own website </p></h2>
-        <p className="text-gray-600 my-5 font-sans">Customize your travel website, and start selling. Let's get your business off the ground with our intuitive tools!</p>
+          <h2 className="text-white text-6xl text-center">
+            Start selling travel today <p>on your own website </p>
+          </h2>
+          <p className="text-gray-600 my-5 font-sans">
+            Customize your travel website, and start selling. Let&apos;s get
+            your business off the ground with our intuitive tools!
+          </p>
         </div>
 
-        
-        <Button variant="secondary" className="text-white w-40  py-5 px-24">Try it now</Button>
-    
+        <Button variant="secondary" className="text-white w-40  py-5 px-24">
+          Try it now
+        </Button>
       </div>
 
-      <div className='bg-white rounded-xl mt-20'>
-       <img src='images/Links Template 13.png' className='sm:h-full '/>
-        
-    </div>
+      <div className="bg-white rounded-xl mt-20">
+        <img src="images/Links Template 13.png" className="sm:h-full " />
+      </div>
     </header>
   );
 }
